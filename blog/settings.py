b2fe229cc87ae290957734728e42a088c45b9c60
Emmanuel Tolulope <princetolu11@gmail.com>
2023-06-24 13:33:17 +0100

    Commit 8

--- blog/settings.py
+++ blog/settings.py
@@ -142,7 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,  'statics')
 #     'https://movie-tsa-react-v4fx-gu4vot7tl-princetolu.vercel.app/',
     
 # ]
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['*']
 
 
 # Base url to serve media files
